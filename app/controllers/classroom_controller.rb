ca90a284ea5bf5987e8b3e4bcf7d1e0158308239
Varun Raj <mailme@varunraj.in>
2015-10-04 19:17:40 +0530

    Classroom Page

--- app/controllers/classroom_controller.rb
+++ app/controllers/classroom_controller.rb
@@ -2,8 +2,9 @@ class ClassroomController < ApplicationController
 
   def index
     @post = Post.new
-    # posts = Post.where(:department_id => current_student.department_id).where(:batch_id => current_student.batch_id).where(:class_section_id => current_student.class_section_id).all
-    @posts = Post.all
+    @posts = Post.where(:department_id => current_student.department_id).where(:batch_id => current_student.batch_id).where(:class_section_id => current_student.class_section_id).all
+    # @posts = Post.all
+    puts @posts.count
   end
 
   def mates
